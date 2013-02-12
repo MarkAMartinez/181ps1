5cc865cb57de499760b9712f27b7b56cfa355da8
j-sun <jsun2510@gmail.com>
2013-02-11 17:43:51 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,121 @@
+# main.py
+# -------
+# Jimmy Sun
+
+from dtree import *
+import sys
+
+class Globals:
+    noisyFlag = False
+    pruneFlag = False
+    valSetSize = 0
+    dataset = None
+
+
+##Classify
+#---------
+def classify(decisionTree, example):
+    return decisionTree.predict(example)
+
+##Learn
+#-------
+def learn(dataset):
+    learner = DecisionTreeLearner()
+    learner.train(dataset)
+    return learner.dt
+
+# main
+# ----
+# The main program loop
+# You should modify this function to run your experiments
+
+def parseArgs(args):
+  """Parses arguments vector, looking for switches of the form -key {optional value}.
+  For example:
+    parseArgs([ 'main.py', '-n', '-p', 5 ]) = { '-n':True, '-p':5 }"""
+  args_map = {}
+  curkey = None
+  for i in xrange(1, len(args)):
+    if args[i][0] == '-':
+      args_map[args[i]] = True
+      curkey = args[i]
+    else:
+      assert curkey
+      args_map[curkey] = args[i]
+      curkey = None
+  return args_map
+
+def validateInput(args):
+    args_map = parseArgs(args)
+    valSetSize = 0
+    noisyFlag = False
+    pruneFlag = False
+    boostRounds = -1
+    maxDepth = -1
+    if '-n' in args_map:
+      noisyFlag = True
+    if '-p' in args_map:
+      pruneFlag = True
+      valSetSize = int(args_map['-p'])
+    if '-d' in args_map:
+      maxDepth = int(args_map['-d'])
+    if '-b' in args_map:
+      boostRounds = int(args_map['-b'])
+    return [noisyFlag, pruneFlag, valSetSize, maxDepth, boostRounds]
+
+
+# k-fold cross validation
+def xvalidate(dataset, nfolds):
+  datasz = len(dataset.examples)/2
+  testsz = datasz/nfolds
+  trainsz = datasz - testsz
+  correct = 0
+
+  for i in range(0, nfolds):
+    # yeah copying is not the best but python is confusing enough as it is
+    trainset = copy.deepcopy(dataset)
+    trainset.examples = trainset.examples[i*testsz : i*testsz + trainsz]
+    dtree = learn(trainset)
+
+    # copy testset and predict
+    testset = trainset
+    testset.examples = dataset.examples[i*testsz + trainsz : i*testsz + datasz]
+    for j in range(0, testsz):
+      # last element of attributes is the actual result
+      if dtree.predict(trainset.examples[i]) == trainset.examples[i].attrs[-1]:
+        correct += 1
+
+  print correct / float(datasz)
+
+def main():
+    arguments = validateInput(sys.argv)
+    noisyFlag, pruneFlag, valSetSize, maxDepth, boostRounds = arguments
+    print noisyFlag, pruneFlag, valSetSize, maxDepth, boostRounds
+
+    # Read in the data file
+    if noisyFlag:
+        f = open("noisy.csv")
+    else:
+        f = open("data.csv")
+
+    data = parse_csv(f.read(), " ")
+    dataset = DataSet(data)
+    
+    # Copy the dataset so we have two copies of it
+    examples = dataset.examples[:]
+
+    dataset.examples.extend(examples)
+    dataset.max_depth = maxDepth
+    if boostRounds != -1:
+      dataset.use_boosting = True
+      dataset.num_rounds = boostRounds
+
+    # ====================================
+    # WRITE CODE FOR YOUR EXPERIMENTS HERE
+    # ====================================
+    xvalidate(dataset, 10)
+
+main()
+
+
+    
